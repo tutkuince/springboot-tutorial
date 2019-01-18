bcf17f6bfad681abe064536b4d0fa571f85e2d0d
tutkuince <tutku.ince@outlook.com>
2019-01-18 16:44:14 +0300

    HelloRestController

---
+++ src/main/java/com/muditasoft/springboottutorial/rest/HelloRestController.java
@@ -0,0 +1,17 @@
+package com.muditasoft.springboottutorial.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+	// expose "/" that return "Hello World"
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on server is: " + LocalDateTime.now();
+	}
+	
+}
