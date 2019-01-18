4e419ff165ee25e68477ff5be992243f5c31e8d4
tutkuince <tutku.ince@outlook.com>
2019-01-18 16:56:59 +0300

    try to understand spring-devtools with HelloRestController with new endpoints

--- src/main/java/com/muditasoft/springboottutorial/rest/HelloRestController.java
+++ src/main/java/com/muditasoft/springboottutorial/rest/HelloRestController.java
@@ -14,4 +14,15 @@ public class HelloRestController {
 		return "Hello World! Time on server is: " + LocalDateTime.now();
 	}
 	
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+	
+	// expose a new endpoint for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day!";
+	}
 }
