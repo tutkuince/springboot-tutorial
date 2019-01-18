04059886b2ac66f917849692db04d0f8129c753e
tutkuince <tutku.ince@outlook.com>
2019-01-18 16:32:23 +0300

    initial  commit

---
+++ src/test/java/com/muditasoft/springboottutorial/SpringbootTutorialApplicationTests.java
@@ -0,0 +1,17 @@
+package com.muditasoft.springboottutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
