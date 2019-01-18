ce7c00a4a14f03d621f9844c5f8de0a0a46b4521
tutkuince <tutku.ince@outlook.com>
2019-01-18 16:45:45 +0300

    informations about @SpringBootApplication

--- src/main/java/com/muditasoft/springboottutorial/SpringbootTutorialApplication.java
+++ src/main/java/com/muditasoft/springboottutorial/SpringbootTutorialApplication.java
@@ -3,7 +3,7 @@ package com.muditasoft.springboottutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	// Composed of @EnableAutoConfiguration, @ComponenetScan, @Configuration
 public class SpringbootTutorialApplication {
 
 	public static void main(String[] args) {
